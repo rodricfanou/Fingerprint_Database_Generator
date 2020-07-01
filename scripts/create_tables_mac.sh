e05adfbe52e382f93e24972e40160245f14c0c03
rodricfanou <roderick@caida.org>
2020-06-30 22:22:59 -0700

    postgres db creation mac

---
+++ scripts/create_tables_mac.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Design a Postgres database schema for storing browser fingerprints
+
+#createdb
+#psql -d postgres
+
+createdb browsersfgp;
+#createdb -U postgres(db user) dbname
+
+## HTTP fingerprints table
+psql  -d browsersfgp -c """CREATE TABLE IF NOT EXISTS http_fgp (http_id INT NOT NULL, http_fgp VARCHAR(500) NOT NULL, PRIMARY KEY (http_id));"""
+## Add infos about the browser?
+
+
+## TLS fingerprints table
+psql  -d browsersfgp -c """CREATE TABLE IF NOT EXISTS tls_fgp (tls_id INT NOT NULL, tls_fgp VARCHAR(500) NOT NULL, PRIMARY KEY (tls_id));"""
+
+
+## cross-reference table
+## Matches the id of a line in http_fgp to the corresponding id in tls_fgp
+psql  -d browsersfgp -c """CREATE TABLE IF NOT EXISTS cross_fgp (http_id INT NOT NULL, tls_id INT NOT NULL, PRIMARY KEY  (http_id, tls_id));"""
